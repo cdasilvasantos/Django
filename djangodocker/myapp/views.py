163e8d407efd947435da199ae137bdcb9d524088
Michael T. Daniels <danielssmichael@gmail.com>
2023-10-31 18:10:19 -0400

    Importing ListView, CreateView, UpdateView, Delete

--- djangodocker/myapp/views.py
+++ djangodocker/myapp/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from .models import Student
 
 # Create your views here.
 
