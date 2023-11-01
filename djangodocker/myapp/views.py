f5c513e9d7ab2695a360c26b03ba2f20511a30f8
cdubbx <124006449+cdubbx@users.noreply.github.com>
2023-11-01 10:44:58 -0400

    I fixed my mistake in views.py

--- djangodocker/myapp/views.py
+++ djangodocker/myapp/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
-
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from .models import Student
 # Create your views here.
 
 def index(request):
     return render(request, 'myapp/index.html')
+
+
+
+
+class StudentListView(ListView):
+    model = Student
+
+class StudentCreateView(CreateView):
+    model = Student
+    fields = ['first_name', 'last_name', 'email']
+
+class StudentUpdateView(UpdateView):
+    model = Student
+    fields = ['first_name', 'last_name', 'email']
+
+class StudentDeleteView(DeleteView):
+    model = Student
+    success_url = reverse_lazy('student_list')
