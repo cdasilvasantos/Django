4e911d640e4a34f54e17c9966de0f22e8dac22e9
cdubbx <124006449+cdubbx@users.noreply.github.com>
2023-11-10 10:54:38 -0500

    changes to view

--- djangodocker/myapp/views.py
+++ djangodocker/myapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from .views import  StudentCreateView, StudentUpdateView, StudentDeleteView
 from .models import Student
 # Create your views here.
 
@@ -8,24 +8,28 @@ def index(request):
     return render(request, 'myapp/index.html')
 
 
+class TodoListView(ListView):
+    model = todoList
+    template_name = 'myapp/todo_list.html'
+    context_object_name = 'todos'
+
+class TodoCreateView(CreateView):
+    model = todoList
+    template_name = 'myapp/todo_form.html'
+    fields = ['this_item', 'time']
+    success_url = reverse_lazy('todo_list')
+
+class TodoUpdateView(UpdateView):
+    model = todoList
+    template_name = 'myapp/todo_form.html'
+    fields = ['this_item', 'time']
+    success_url = reverse_lazy('todo_list')
+
+class TodoDeleteView(DeleteView):
+    model = todoList
+    template_name = 'myapp/todo_confirm_delete.html'
+    success_url = reverse_lazy('todo_list')
 
 
-class StudentListView(ListView):
-    model = Student
 
-class StudentCreateView(CreateView):
-    model = Student
-    fields = ['first_name', 'last_name', 'email']
-    template_name = 'myapp/student_list.html'  # Specify your template name here
-    success_url = reverse_lazy('student_list')
 
-class StudentUpdateView(UpdateView):
-    model = Student
-    fields = ['first_name', 'last_name', 'email']
-    template_name = 'myapp/student_list.html'  # Specify your template name here
-    success_url = reverse_lazy('student_list')
-
-class StudentDeleteView(DeleteView):
-    model = Student
-    template_name = 'myapp/student_list.html'  # Specify your template name here
-    success_url = reverse_lazy('student_list')  # Use the name of your list view url
