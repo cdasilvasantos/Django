6e7764b182cf59ad9368783153add465db326d06
cdubbx <124006449+cdubbx@users.noreply.github.com>
2023-11-10 10:57:39 -0500

    added changes

--- djangodocker/myapp/models.py
+++ djangodocker/myapp/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
-class Student(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.EmailField(unique=True)
+# class Student(models.Model):
+#     first_name = models.CharField(max_length=50)
+#     last_name = models.CharField(max_length=50)
+#     email = models.EmailField(unique=True)
 
 class todoList(models.Model):
     this_item = models.CharField(max_length=50)
     time = models.CharField(max_length=50)
 
-    
+
