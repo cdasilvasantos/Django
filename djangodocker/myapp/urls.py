d60cd7fcf1cf501dc2c291a40503c81052f8e3b2
cdasilvasantos <cd58@njit.edu>
2023-11-17 00:02:00 -0500

    updated url

--- djangodocker/myapp/urls.py
+++ djangodocker/myapp/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from .views import TodoListView, TodoCreateView, TodoUpdateView, TodoDeleteView, index, todo_view, add_todo_view
+from .views import todo_view, add_todo_view
+
+
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('todos/', TodoListView.as_view(), name='todo_list'),
-    path('todo/<int:pk>/', TodoUpdateView.as_view(), name='todo_update'),
-    path('todo/create/', TodoCreateView.as_view(), name='todo_create'),
-    path('todo/<int:pk>/delete/', TodoDeleteView.as_view(), name='todo_delete'),
-    path('todo/', todo_view, name='todo_view'),
-    path('add_todo/', add_todo_view, name='add_todo_view'),
+    path('', todo_view, name='todo_view'),
+     path('add/', add_todo_view, name='todo_add'),   
+    path('todo/<int:pk>/', todo_view, name='todo_view'),
 ]
+
